cf411c66f69e691f4b946be91a9d7d9a0b1488a2
natalia559 <natalia.rosa.mariano@escola.pr.gov.br>
2026-07-03 14:05:06 -0300

    oi

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,24 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'robotica',
+    'o que é Qual o papel da lógica de programação?',
+    'É a organização sequencial dos comandos (passo a passo) para atingir um objetivo ou resolver um problema'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'robotica,
+    'O que é um código ou programa em robótica?',
+    'É um conjunto de instruções escritas em uma linguagem específica que diz ao robô o que ele deve fazer.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'fisíca',
+    'O que é a gravidade?',
+    'A força que atrai objetos com massa entre si.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'fisíca',
+    'O que diz a Primeira Lei de Newton?',
+    'Um objeto em repouso permanece em repouso se não houver força externa.'
 )
 
